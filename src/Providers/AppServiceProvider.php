b802599aa5ee8cb5dbd56a4c072b41e3a0cbc823
Mitch Hijlkema <6103741+pridestalker@users.noreply.github.com>
2019-10-31 14:49:21 +0100

    [release]
    - Added static caching for files
    - Added logo component
    - Added basic header template
    - Added post tease template
    - Added front-page template
    - Added theme support for post-thumbnails; html5
    - Added functions|mixins|variables for screen sizes
    - Added default typography stylesheets
    - Added default card styling
    - Added default header styling
    - Added default post tease styling
    
    Signed-off-by: Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace App\Providers;
 
 use App\Helpers\WP;
 
 class AppServiceProvider
 {
-	protected $providers;
-	public function __construct()
-	{
-		$providers = include WP::getStylesheetDir() . '/src/config/app.php';
-		$this->providers = $providers['providers'];
-		$this->boot();
-		$this->register();
-	}
-	
-	public function boot(): void
-	{
-		foreach ($this->providers as $provider) {
-			new $provider();
-		}
-		
-		do_action('webspin/providers/registered');
-	}
-	
-	public function register(): void
+    protected $providers;
+    public function __construct()
+    {
+        $providers = include WP::getStylesheetDir() . '/src/config/app.php';
+        $this->providers = $providers['providers'];
+        $this->boot();
+        $this->register();
+    }
+    
+    public function boot(): void
+    {
+        foreach ($this->providers as $provider) {
+            new $provider();
+        }
+        do_action('webspin/providers/registered');
+        
+        add_theme_support('post-thumbnails');
+        add_theme_support('html5');
+    }
+    
+    public function register(): void
     {
         $this->updateChecker();
         do_action('webspin/app/registered');
