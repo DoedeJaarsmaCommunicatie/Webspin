deb0a0d9a881368dfdb7a3b7d55fcf51bc569f8a
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-11-06 16:10:45 +0100

    Added bootstrap.js to bootstrap js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,13 @@
+import './bootstrap';
 import $ from 'jquery';
 import Router from './tools/Router';
 
 import common from './routes/Common';
 import home from './routes/Home';
 
-__webpack_public_path__ = document.head.querySelector('meta[name="base_assets"]').content;
-
 const router = new Router({
     common,
     home
 });
-
-window.router = router;
 
 $(document).ready(() => router.loadEvents());
