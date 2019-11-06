76e67d864a378e96beabdf9d9fc7c1025e8ba37e
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-11-06 14:56:28 +0100

    Made dynamic imports even more dynamic with dynamic theme URL (#9)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,10 @@
 import $ from 'jquery';
 import Router from './tools/Router';
-// const common = () =>
-//     import(/* webpackChunkName: "dist/scripts/common" */'./routes/Common')
-//         .then(res => routes.common = res);
-//
-// const home = () =>
-//     import(/* webpackChunkName: "dist/scripts/home" */ './routes/Home')
-//         .then(res => routes.home = res);
 
 import common from './routes/Common';
 import home from './routes/Home';
+
+__webpack_public_path__ = document.head.querySelector('meta[name="base_assets"]').content;
 
 const router = new Router({
     common,
